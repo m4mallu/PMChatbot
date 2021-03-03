07fcc18c1c315df08e841b68bedc1b4a6813f5fc
m4mallu <renju700@gmail.com>
2021-03-03 13:57:07 +0530

    Small Changes

--- sample_config.py
+++ sample_config.py
@@ -3,6 +3,6 @@ import os
 
 class Config(object):
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN")
-    APP_ID = os.environ.get("APP_ID")
-    API_HASH = int(os.environ.get("API_HASH"))
+    APP_ID = int(os.environ.get("APP_ID"))
+    API_HASH = os.environ.get("API_HASH")
     ADMIN = int(os.environ.get("ADMIN"))
