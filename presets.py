9840c4a24d857e4fce683433fc2c41f74d6f8ec6
aylak-github <85997548+aylak-github@users.noreply.github.com>
2021-08-20 11:03:40 +0300

    update presets.py

--- presets.py
+++ presets.py
@@ -1,6 +1,9 @@
-#-------------------------------------- https://github.com/m4mallu/PMChatbot ------------------------------------------#
+#-------------------------------------- https
+# https://github.com/aylak-github/PMChatbot
+
+ # Forked ://github.com/m4mallu/PMChatbot ------------------------------------------#
 class Presets(object):
     WELCOME_TEXT = "Merhaba <b>{}</b>! \n<code>Sorununuzu buraya yazın..\n En erken yanıt verilecektir </code> 😉"
-    USER_DETAILS = "<b>Yeni Mesaj:</b>\nİsim: {} {}\nID: {}\nKullanıcı Adı: @{}\nScam: {}\nKısıtlanmış mı? : {}\nDurum: {}\nDc Id: {}"
-    PM_TXT_ATT = "<b>Şundan İletildi:</b> {}\n<b>Name:</b> {}\n\n{}"
-    PM_MED_ATT = "<b>Şundan İletildi:</b> {} \n<b>Name:</b> {}"
+    USER_DETAILS = "<b>Yeni Mesaj:</b>\nİsim: {} {}\nID: {}\nKullanıcı Adı: @{}\nScam: {}\nKısıtlanmış mı? : {}\nDurum: {}\nDC Kimliği: {}"
+    PM_TXT_ATT = "<b>Şundan İletildi:</b> {}\n<b>İsim:</b> {}\n\n{}"
+    PM_MED_ATT = "<b>Şundan İletildi:</b> {} \n<b>İsim:</b> {}"
