e2d3ba0d508ba9ae97af61162fb2b73998455693
aylak-github <85997548+aylak-github@users.noreply.github.com>
2021-08-20 10:25:25 +0300

    update presets.py

--- presets.py
+++ presets.py
@@ -1,6 +1,6 @@
 #-------------------------------------- https://github.com/m4mallu/PMChatbot ------------------------------------------#
 class Presets(object):
-    WELCOME_TEXT = "Hello.. <b>{}</b>\n<code>Sorununuzu buraya yazın..\n En erken yanıt verilecektir </code> 😉"
-    USER_DETAILS = "<b>Yeni Mesaj:</b>\nİsim: {} {}\nID: {}\nKullanıcı Adı: @{}\nScam: {}\nKısıtlanmış mı? : {}\nStatus: {}\nDc Id: {}"
-    PM_TXT_ATT = "<b>Message from:</b> {}\n<b>Name:</b> {}\n\n{}"
-    PM_MED_ATT = "<b>Message from:</b> {} \n<b>Name:</b> {}"
+    WELCOME_TEXT = "Merhaba <b>{}</b>! \n<code>Sorununuzu buraya yazın..\n En erken yanıt verilecektir </code> 😉"
+    USER_DETAILS = "<b>Yeni Mesaj:</b>\nİsim: {} {}\nID: {}\nKullanıcı Adı: @{}\nScam: {}\nKısıtlanmış mı? : {}\nDurum: {}\nDc Id: {}"
+    PM_TXT_ATT = "<b>Şundan İletildi:</b> {}\n<b>Name:</b> {}\n\n{}"
+    PM_MED_ATT = "<b>Şundan İletildi:</b> {} \n<b>Name:</b> {}"
